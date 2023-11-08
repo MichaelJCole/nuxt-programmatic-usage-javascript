351337f2f93e05d3c7fa66de257bdf1271c9ea3a
Michael Cole <gitlab@michaelcole.com>
2023-11-08 15:44:30 -0600

    fixed

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 class ProgrammaticUsage {
   async generateSite() {
     const { resolve } = await import("path");
-    const { build, loadNuxt } = await import("nuxt");
+    const { buildNuxt, loadNuxt } = await import("@nuxt/kit");
     const { defineNuxtConfig } = await import("nuxt/config");
 
     let nuxt = undefined;
 
     const config = defineNuxtConfig({
-      rootDir: resolve(__dirname, "rootDir"),
       devtools: { enabled: true },
       ssr: false,
-      buildDir: resolve(__dirname, "buildDir"),
-      generate: { routes: ["/"] },
+      modules: ["@vite-pwa/nuxt"], // https://vite-pwa-org.netlify.app/frameworks/nuxt
+      pwa: {
+        manifest: {}, // set to generate manifest.webmanifest
+        workbox: {
+          navigateFallback: "/",
+          globPatterns: ["**/*.{js,css,html,json,ico,png,svg}"], // <== json files included: when offline you will see missing json files request
+        },
+      },
+      generate: {
+        routes: ["/"],
+        // TODO https://vite-pwa-org.netlify.app/frameworks/nuxt#prompt-for-update-and-offline-ready
+      },
     });
+
     const options = {
       dev: false,
       ready: true,
-      rootDir: resolve(__dirname, "rootDir"),
-      config,
+      cwd: resolve(__dirname, "rootDir"),
+      overrides: config,
     };
     console.log("--------------------------------------> options", options);
 
     if (!nuxt) nuxt = await loadNuxt(options);
-    await nuxt.ready(); // Necessary?
     console.log("--------------------------------------> loadNuxt complete");
 
-    await build(nuxt);
+    await buildNuxt(nuxt);
     console.log("--------------------------------------> build complete");
   }
 }
